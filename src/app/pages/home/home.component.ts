8f28a585368c00e8e7b4f2715824025ae7551caf
Byron <byron16garcia@gmail.com>
2025-05-19 23:16:53 -0600

    Nuevo diseño 2.0

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,42 +1,56 @@
 import { Component } from '@angular/core';
-import { ChatbotComponent } from '../../components/chatbot/chatbot.component';
 import { CommonModule } from '@angular/common';
+import type { SwiperOptions } from 'swiper/types'; // Importa solo el tipo
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ChatbotComponent, CommonModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
- productos = [
+  productos = [
     {
       titulo: 'Bodies para recién nacidos',
       descripcion: 'Suaves y seguros para los primeros días de tu bebé.',
-      imagen: 'assets/images/ropa1.jpg'
+      imagen: 'assets/images/ropa1.jpg',
+      precio: 12.99
     },
     {
       titulo: 'Ropa de juego',
       descripcion: 'Confortable y resistente para sus primeras aventuras.',
-      imagen: 'assets/images/ropa2.jpg'
+      imagen: 'assets/images/ropa2.jpg',
+      precio: 18.49
     },
     {
       titulo: 'Accesorios',
       descripcion: 'Gorros, baberos y más para complementar cada look.',
-      imagen: 'assets/images/ropa3.jpg'
+      imagen: 'assets/images/ropa3.jpg',
+      precio: 7.5
     },
     {
       titulo: 'Conjuntos casuales',
       descripcion: 'Estilos modernos y tiernos para cualquier ocasión.',
-      imagen: 'assets/images/ropa4.jpg'
+      imagen: 'assets/images/ropa4.jpg',
+      precio: 22.0
     },
     {
       titulo: 'Pijamas suaves',
       descripcion: 'Para un descanso cálido y cómodo.',
-      imagen: 'assets/images/ropa5.jpg'
+      imagen: 'assets/images/ropa5.jpg',
+      precio: 15.75
     }
   ];
 
+  swiperConfig: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: true,
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 3 }
+    }
+  };
 }
