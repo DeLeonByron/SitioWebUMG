3238a6c72deccb03b47acd964af1740e146745bc
Byron <byron16garcia@gmail.com>
2025-05-20 22:25:42 -0600

    Diseño final

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,23 +16,18 @@ export class HomeComponent {
   constructor(private renderer: Renderer2) {}
 
   productos = [
-    { titulo: 'Body Rosa', descripcion: 'Algodón suave 100%', imagen: 'assets/img/body-rosa.jpg', precio: 12.99 },
-    { titulo: 'Pijama Osito', descripcion: 'Ideal para dormir cómodo', imagen: 'assets/img/pijama-osito.jpg', precio: 18.50 },
-    { titulo: 'Conjunto Bebé', descripcion: 'Incluye pantalón y camiseta', imagen: 'assets/img/conjunto.jpg', precio: 24.99 },
-    { titulo: 'Vestido Flores', descripcion: 'Perfecto para ocasiones especiales', imagen: 'assets/img/vestido.jpg', precio: 21.00 },
-    { titulo: 'Gorro Tejido', descripcion: 'Calidez para tu bebé', imagen: 'assets/img/gorro.jpg', precio: 8.75 },
-    { titulo: 'Zapatos Suaves', descripcion: 'Confort y estilo', imagen: 'assets/img/zapatos.jpg', precio: 15.20 },
+    { titulo: 'Body Rosa', descripcion: 'Algodón suave 100%', imagen: 'assets/images/ropa1.jpg', precio: 12.99 },
+    { titulo: 'Pijama Osito', descripcion: 'Ideal para dormir cómodo', imagen: 'assets/images/ropa1.jpg', precio: 18.50 },
+    { titulo: 'Conjunto Bebé', descripcion: 'Incluye pantalón y camiseta', imagen: 'assets/images/ropa1.jpg', precio: 24.99 },
+    { titulo: 'Vestido Flores', descripcion: 'Perfecto para ocasiones especiales', imagen: 'assets/images/ropa1.jpg', precio: 21.00 },
+    { titulo: 'Gorro Tejido', descripcion: 'Calidez para tu bebé', imagen: 'assets/images/ropa1.jpg', precio: 8.75 },
+    { titulo: 'Zapatos Suaves', descripcion: 'Confort y estilo', imagen: 'assets/images/ropa1.jpg', precio: 15.20 },
   ];
 
   grupos: any[] = [];
 
   ngOnInit(): void {
     this.grupos = this.crearGrupos(this.productos, 3);
-
-     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (prefersDark) {
-      this.renderer.setAttribute(document.body, 'data-theme', 'dark');
-    }
   }
 
   crearGrupos(arr: any[], tam: number): any[] {
@@ -41,9 +36,5 @@ export class HomeComponent {
       return acc;
     }, []);
   }
-
-  toggleDarkMode(event: any) {
-      this.isDarkMode = event.target.checked;
-    }
 
 }
