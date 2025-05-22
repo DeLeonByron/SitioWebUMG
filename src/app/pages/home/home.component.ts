5e4ca1986763a85c9372b00c895e933b5a8023ad
Byron <byron16garcia@gmail.com>
2025-05-21 21:58:44 -0600

    Se agrega nuevas imagenes de diseño de ropa, se agrega favicon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Renderer2 } from '@angular/core';
+import { Component, HostBinding, HostListener, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatbotComponent } from '../../components/chatbot/chatbot.component';
 
@@ -18,24 +18,33 @@ export class HomeComponent {
 
   productos = [
     { titulo: 'Body Rosa', descripcion: 'Algodón suave 100%', imagen: 'assets/images/ropa1.jpg', precio: 12.99 },
-    { titulo: 'Pijama Osito', descripcion: 'Ideal para dormir cómodo', imagen: 'assets/images/ropa1.jpg', precio: 18.50 },
-    { titulo: 'Conjunto Bebé', descripcion: 'Incluye pantalón y camiseta', imagen: 'assets/images/ropa1.jpg', precio: 24.99 },
-    { titulo: 'Vestido Flores', descripcion: 'Perfecto para ocasiones especiales', imagen: 'assets/images/ropa1.jpg', precio: 21.00 },
-    { titulo: 'Gorro Tejido', descripcion: 'Calidez para tu bebé', imagen: 'assets/images/ropa1.jpg', precio: 8.75 },
-    { titulo: 'Zapatos Suaves', descripcion: 'Confort y estilo', imagen: 'assets/images/ropa1.jpg', precio: 15.20 },
+    { titulo: 'Pijama Osito', descripcion: 'Ideal para dormir cómodo', imagen: 'assets/images/ropa2.jpg', precio: 18.50 },
+    { titulo: 'Conjunto Bebé', descripcion: 'Incluye pantalón y camiseta', imagen: 'assets/images/ropa3.jpg', precio: 24.99 },
+    { titulo: 'Vestido Flores', descripcion: 'Perfecto para ocasiones especiales', imagen: 'assets/images/ropa4.jpg', precio: 21.00 },
+    { titulo: 'Gorro Tejido', descripcion: 'Calidez para tu bebé', imagen: 'assets/images/ropa5.jpg', precio: 8.75 },
+    { titulo: 'Zapatos Suaves', descripcion: 'Confort y estilo', imagen: 'assets/images/ropa6.jpg', precio: 15.20 },
+    { titulo: 'Zapatos Suaves', descripcion: 'Confort y estilo', imagen: 'assets/images/ropa7.jpg', precio: 15.20 },
   ];
 
-  grupos: any[] = [];
+  gruposProductos: any[][] = [];
 
   ngOnInit(): void {
-    this.grupos = this.crearGrupos(this.productos, 3);
+    this.generarGruposProductos();
   }
 
-  crearGrupos(arr: any[], tam: number): any[] {
-    return arr.reduce((acc, val, i) => {
-      if (i % tam === 0) acc.push(arr.slice(i, i + tam));
-      return acc;
-    }, []);
+  @HostListener('window:resize')
+  onResize() {
+    this.generarGruposProductos();
+  }
+
+  generarGruposProductos(): void {
+    const anchoPantalla = window.innerWidth;
+    const tamañoGrupo = anchoPantalla >= 992 ? 3 : 1;
+
+    this.gruposProductos = [];
+    for (let i = 0; i < this.productos.length; i += tamañoGrupo) {
+      this.gruposProductos.push(this.productos.slice(i, i + tamañoGrupo));
+    }
   }
 
 }
